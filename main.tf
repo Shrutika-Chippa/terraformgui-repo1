a3abef104563ada183cc6c1d48b2ec674894b220
Shrutika chippa <83702085+Shrutika-Chippa@users.noreply.github.com>
2026-07-08 13:38:06 +0530

    Change instance count and update tag name

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ provider "aws" {
   region = "ap-south-1"
   }
 resource "aws_instance" "name" {
+   count = 2
    ami           = "ami-01a18c38ece67e620"
    instance_type = "t3.micro"
 
    tags = {
-     Name =  "dev-ins"
+     Name =  "prod-ins"
    }
 }
